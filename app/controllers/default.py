a22467a6d67546791beb8432dc91b05faaf27cc5
Murillo Domingos <66131893+murillodomingos@users.noreply.github.com>
2020-12-01 15:25:35 -0300

    Revert "add docker structure and move sqllite to postgres"

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from app import app
 
+
 @app.route('/index/<user>')
 @app.route('/', defaults={'user':None})
 def index(user):
--- docker/postgres/V000__setup.sql
+++
@@ -1,5 +0,0 @@
-CREATE ROLE "organizzare-org" WITH SUPERUSER;
-
-CREATE USER "organizzare-user" WITH PASSWORD 'organizzare-pass' IN ROLE "organizzare-org";
-
-CREATE DATABASE "organizzare" WITH OWNER "organizzare-back";
