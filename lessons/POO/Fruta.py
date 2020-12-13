79324684d9319cb19e64a530bf44d96c8097f13f
Gustavo <gubtosmail@gmail.com>
2020-12-13 16:01:03 -0300

    add lesson 1

---
+++ lessons/POO/Fruta.py
@@ -0,0 +1,19 @@
+def get_price(peso_g, preco_kg):   # <- funcao
+    return peso_g * preco_kg
+
+class Fruta: # <- classe
+    def __init__(self, nome, peso_g, preco_kg): # <- construtor
+        self.nome = nome # <- atributo
+        self.peso_g = peso_g
+        self.preco_kg = preco_kg
+
+    def get_price(self): # <- metodo
+        return "R$ %.2f"%( self.peso_g * self.preco_kg / 1000)
+
+    def get_pounds(self):
+        return "%d pounds"%(self.peso_g * 236)
+
+medida_1 = Fruta("maçã", 200, 1.00) # <- objeto (instancia da classe usando o construtor)
+
+print(medida_1.get_price()) # <- chamada do metodo
+print(medida_1.nome) # <- veficacao do atributo
---
+++ lessons/POO/readme.md
@@ -0,0 +1,3 @@
+POO é programacação orientada a objetos (um estilo de programação)
+
+Veja o arquivo Fruta.py
