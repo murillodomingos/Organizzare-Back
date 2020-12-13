fce8d12090d8105882f89c00771ea99c604837ac
Murillo <murillodomingos516@gmail.com>
2020-12-13 16:31:17 -0300

    create-dao2

---
+++ organizzare-back-v2/dao/connection.py
@@ -0,0 +1,6 @@
+from sqlalchemy import create_engine
+
+def get_connection():
+    engine = create_engine('postgresql://organizzare-user:organizzare-pass@localhost:5432/organizzare')
+
+    return engine.connect()
